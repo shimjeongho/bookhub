33754b2d60ca2ee9fa47fb12fca7db0bdfdafcf4
dglee0310 <dglee0310@gmail.com>
2025-04-28 15:45:55 +0900

    250428 15:45 Add User 관리객체 vo 추가

--- src/main/java/kr/co/bookhub/vo/User.java
+++ src/main/java/kr/co/bookhub/vo/User.java
@@ -3,6 +3,17 @@ package kr.co.bookhub.vo;
 
 import java.util.Date;
 
+import org.apache.ibatis.type.Alias;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Alias("User")
+
 public class User {
 	private String id;
 	private String password;
