963bfcaa469102876f60c30f442c2857811a0629
jun-geun-choi <jklucky73@gmail.com>
2025-05-23 10:54:43 +0900

    dasfdds

---
+++ sql/bookhub_library.sql
@@ -0,0 +1,26 @@
+
+insert into bookhub_libraries (
+lib_no, lib_name, lib_location,  lib_latitude,lib_longitude, lib_tel, lib_img_path, lib_business_hours)
+values
+(BOOKHUB_LIBRARIES_LIB_NO_SEQ.nextval,'LG상남도서관','서울특별시 종로구 창덕궁1길 18','37.579755', '126.988438','02-708-3700','https://www.lg.or.kr/web/images/about/img_about_top_600.jpg','09:00-18:00');
+
+insert into bookhub_libraries (
+lib_no, lib_name, lib_location,  lib_latitude,lib_longitude, lib_tel, lib_img_path, lib_business_hours)
+values
+(BOOKHUB_LIBRARIES_LIB_NO_SEQ.nextval,'우리소리도서관','서울특별시 종로구 삼일대로30길 47','37.574897', '126.990232','070-4550-5015','https://www.smalllibrary.org/contents/2024/01/19/30a344b4-f55b-4707-8ef0-905d419e98b8.jpg','09:00-18:00');
+
+insert into bookhub_libraries (
+lib_no, lib_name, lib_location,  lib_latitude,lib_longitude, lib_tel, lib_img_path, lib_business_hours)
+values
+(BOOKHUB_LIBRARIES_LIB_NO_SEQ.nextval,'서울특별시교육청 정독도서관','서울특별시 종로구 북촌로5길 48','37.581381', '126.983333','02-2011-5799','https://upload.wikimedia.org/wikipedia/commons/7/7b/Jeongdok_public_library.jpg','09:00-22:00');
+
+insert into bookhub_libraries (
+lib_no, lib_name, lib_location,  lib_latitude,lib_longitude, lib_tel, lib_img_path, lib_business_hours)
+values
+(BOOKHUB_LIBRARIES_LIB_NO_SEQ.nextval,'서울특별시교육청 어린이도서관','서울특별시 종로구 사직로9길 7','37.576904', '126.968144','02-731-2300','https://mblogthumb-phinf.pstatic.net/20160709_196/intiend_14680668459307XhwT_JPEG/IMG_4535.jpg?type=w800','09:00-18:00');
+
+insert into bookhub_libraries (
+lib_no, lib_name, lib_location,  lib_latitude,lib_longitude, lib_tel, lib_img_path, lib_business_hours)
+values
+(BOOKHUB_LIBRARIES_LIB_NO_SEQ.nextval,'삼청공원숲속도서관','서울특별시 종로구 북촌로 134-3','37.588948', '126.984127','02-734-3900','https://mediahub.seoul.go.kr/wp-content/uploads/2019/03/2da58215ad909c7807d5b0f5fd15467b.jpg','10:00-18:00');
+commit;
